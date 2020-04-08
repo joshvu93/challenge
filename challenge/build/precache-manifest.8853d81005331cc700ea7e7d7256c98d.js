ab6d4beedcf9336bb31a557cfdf06f8c1ceca2da
Joshvu93 <joshvu93@gmail.com>
2020-04-07 17:10:40 -0700

    wip

---
+++ challenge/build/precache-manifest.8853d81005331cc700ea7e7d7256c98d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "957cfc765d1c4fbc8f1b1deaffb9d3e6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f2b2ba25b1905cd930a6",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "0f4ae63895e9aa7bffaa",
+    "url": "/static/js/2.b6936792.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b6936792.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2b2ba25b1905cd930a6",
+    "url": "/static/js/main.eb41d6d7.chunk.js"
+  },
+  {
+    "revision": "593cabece420f295b566",
+    "url": "/static/js/runtime-main.5946bd76.js"
+  }
+]);
